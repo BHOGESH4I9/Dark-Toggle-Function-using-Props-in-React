8b67ca71f8d3171474bd750a1bbc3b853665210a
BHOGESH4I9 <vurabhogesh@gmail.com>
2025-06-19 06:53:54 -0400

    Toggle Theme Component was changed with bootstrap classes

--- src/components/Footer/ThemeToggle.jsx
+++ src/components/Footer/ThemeToggle.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
 const ThemeToggle = ({theme, setTheme}) => {
+  const toggleFun = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    console.log('Theme Changed');
+  };
 
-    const toogleFun = () => {
-        const newTheme = theme === 'light' ? 'dark' : 'light';
-        setTheme(newTheme);
-        console.log('Theme Changed');
-    }
   return (
-    <div>
-        <button onClick={toogleFun}>Chanege to dark and light mode</button>
+    <div className="toggle-container">
+      <button onClick={toggleFun}>
+        Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default ThemeToggle
