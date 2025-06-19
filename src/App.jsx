35e87db4a32f7a65bbcb839a10ae3f88df2c86aa
BHOGESH4I9 <vurabhogesh@gmail.com>
2025-06-19 06:50:13 -0400

    Add Bootstrap classes to the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,11 @@ import Footer from "./components/Footer/Footer";
 function App() {
   const [theme, setTheme] = useState("light");
 
-  const styles = {
-    light: {
-      backgroundColor: "white",
-      color: "black",
-    },
-    dark: {
-      backgroundColor: "#1a1a1a",
-      color: "white",
-    },
-  };
 
   return (
     <>
-      <div
-        style={{
-          ...styles[theme],
-          fontFamily: "Arial",
-          minHeight: "100vh",
-          padding: "20px",
-        }}
-      >
-        <h1>Dark Toggle Function using Props in React</h1>
+      <div className={`container ${theme === 'light' ? 'light-theme' : 'dark-theme'}`}>
+        <h1 className="validation-message success">Theme Toggle Demo in React</h1>
         <Header />
         <Footer theme={theme} setTheme={setTheme} />
       </div>
